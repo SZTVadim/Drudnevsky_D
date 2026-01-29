784c579e5f521cea4898b644f685389c4119b967
DRudnev <dmitrurudnev@gmail.com>
2026-01-29 15:09:41 +0300

    delet print

--- homework/lesson_12.py
+++ homework/lesson_12.py
@@ -8,13 +8,12 @@ employees = {
 
 # 1. Добавьте нового сотрудника "Анна"
 employees["Анна"] = {"возраст": 28, "отдел": "HR", "зарплата": 65000}
-# print(employees)
+
 # 2 Создайте список всех имен сотрудников
 list1 = list(employees.keys())
-# print(list1)
+
 # 3. Найдите среднюю зарплату всех сотрудников
-# list2 = list(employees.values())
-# print(list2)
+
 list_zp = [zp["зарплата"] for zp in employees.values()]
 salary_avg = sum(list_zp) / len(list_zp)
 print(list_zp)
